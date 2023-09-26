628ce35d8899bb91a7598a40b93a748420ff3f4b
Joshua Huang <joshuahuang@rayark.com>
2023-09-26 03:39:46 +0000

    Optimize material collection

--- src/Algorithm.hpp
+++ src/Algorithm.hpp
@@ -6,6 +6,7 @@ void SimpleBFS(Botcraft::BehaviourClient& c);
 void SimpleDFS(Botcraft::BehaviourClient& c);
 void SliceDFS(Botcraft::BehaviourClient& c);
 
+// Used for material collecting optimization
 struct MaterialCompare {
   bool operator()(const std::string& lhs, const std::string& rhs) const {
     if (lhs.find("_wool") != std::string::npos && rhs.find("_wool") != std::string::npos) {
@@ -18,19 +19,22 @@ struct MaterialCompare {
       // only right has "_wool", put at front
       return false;
     } else if (lhs.find("_terracotta") != std::string::npos && rhs.find("_terracotta") != std::string::npos) {
-      // If both have "_terracotta", sort by default
-      return lhs < rhs; // 如果两者都包含 "_terracotta"，按字母顺序排列
+      return lhs < rhs;
     } else if (lhs.find("_terracotta") != std::string::npos) {
-      // only left has "_terracotta", put at front
       return true;
     } else if (rhs.find("_terracotta") != std::string::npos) {
-      // only right has "_terracotta", put at front
       return false;
     } else if (lhs.find("_block") != std::string::npos && rhs.find("_block") != std::string::npos) {
       return lhs < rhs;
     } else if (lhs.find("_block") != std::string::npos) {
       return true;
     } else if (rhs.find("_block") != std::string::npos) {
+      return false;
+    } else if (lhs.find("_planks") != std::string::npos && rhs.find("_planks") != std::string::npos) {
+      return lhs < rhs;
+    } else if (lhs.find("_planks") != std::string::npos) {
+      return true;
+    } else if (rhs.find("_planks") != std::string::npos) {
       return false;
     } else {
       // sort by default

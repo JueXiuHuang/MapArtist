9de89552fbc16b8a314d4bed3c56a2e7a4216c25
JueXiuHuang <joshua87719@gmail.com>
2023-10-14 23:37:26 +0800

    fix multiplayer mode bug

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -273,7 +273,7 @@ void SliceDFS(BehaviourClient& c) {
   const Position size = end - start + Position(1, 1, 1);
   vector<vector<vector<bool>>> visited(size.x, vector<vector<bool>>(size.y, vector<bool>(size.z, false)));
 
-  const int maxWorker = blackboard.Get<int>("maxWorker", 1);
+  const int workerNum = blackboard.Get<int>("workerNum", 1);
   const int workCol = blackboard.Get<int>("workCol", 0);
 
   int slotCounter = 0;
@@ -287,7 +287,7 @@ void SliceDFS(BehaviourClient& c) {
                                             Position(0, 0, 1), Position(0, 0, -1)});
 
   for (int x = xCheckStart; x < size.x; x++) {
-    if (x%maxWorker != workCol) continue;
+    if (x%workerNum != workCol) continue;
     // Put every y=0 blocks to pending stack
     for (int z = 0; z < size.z; z++) {
         pending.push(Position(x, 0, z));

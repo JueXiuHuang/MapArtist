0a506c57d6d12828098a210d2d81a3cd39c93c4e
JueXiuHuang <joshua87719@gmail.com>
2023-10-18 21:41:55 +0800

    refactor custom commands and add move command

--- src/Artist.cpp
+++ src/Artist.cpp
@@ -1,5 +1,7 @@
 #include "Artist.hpp"
 #include "CustomSubTree.hpp"
+#include "CustomTask.hpp"
+#include "Regex.hpp"
 #include "Utils.hpp"
 #include "botcraft/AI/BehaviourTree.hpp"
 #include "botcraft/AI/Tasks/AllTasks.hpp"
@@ -19,94 +21,99 @@ using namespace std;
 using json = nlohmann::json;
 
 void cmdHandler(string cmd, Artist *artist) {
-  if (cmd == "hungry") {
+  smatch matches;
+  if (regex_search(cmd, matches, HungryPattern)) {
     Status s = IsHungry(*artist, 15);
+
     cout << GetTime() << "Current food: " << artist->GetEntityManager()->GetLocalPlayer()->GetFood() << endl;
     if (s == Status::Success) {
       artist->SendChatMessage("I'm hungry.");
     } else {
       artist->SendChatMessage("I'm not hungry.");
     }
-  } else if (cmd == "stop") {
+  } else if (regex_search(cmd, matches, StopPattern)) {
     artist->SendChatMessage("=== BOT STOP ===");
     artist->SetBehaviourTree(nullptr);
-  } else if (cmd == "start") {
+  } else if (regex_search(cmd, matches, StartPattern)) {
     Blackboard& bb = artist->GetBlackboard();
     int workerNum = bb.Get<int>("workerNum", 1);
     int workCol = bb.Get<int>("workCol", 0);
+
     cout << GetTime() << "=== BOT START ===" << endl;
     artist->SendChatMessage("=== BOT START ===");
     artist->SetBehaviourTree(FullTree(), {{"configPath", artist->configPath},
                                           {"pathFinder", artist->finder}, 
                                           {"workerNum", workerNum},
                                           {"workCol", workCol}});
-  } else if (cmd == "bar") {
+  } else if (regex_search(cmd, matches, BarPattern)) {
     int xCheckStart = artist->GetBlackboard().Get<int>("SliceDFS.xCheckStart", 0);
     int ratio = (xCheckStart + 1) * 20 / 128;
     double percent = static_cast<double>(xCheckStart + 1) * 100 / 128;
+
     ostringstream bar;
     bar << "[" << string(ratio, '#') << string(20 - ratio, '-') << "]  "
         << fixed << setprecision(1) << percent << "%";
     artist->SendChatMessage(bar.str());
-  } else if (cmd == "csafe") {
+  } else if (regex_search(cmd, matches, CsafePattern)) {
     artist->SendChatCommand(cmd);
-  } else if (cmd.find("cmd") != string::npos) {
-    regex pattern("cmd\\s+(.+)");
-    smatch matches;
-    regex_search(cmd, matches, pattern);
-    string ingameCmd = matches[1];
-    artist->SendChatCommand(ingameCmd);
-  } else if (cmd == "name") {
-    string name = artist->GetNetworkManager()->GetMyName();
-    artist->SendChatMessage(name);
-  } else if (cmd.find("setCol") != string::npos) {
-    string name = artist->GetNetworkManager()->GetMyName();
-    regex patternAssign("([^\\s]+) ([^\\s]+) (\\d+)");
-    smatch matches;
-    if (regex_search(cmd, matches, patternAssign)) {
-      string command = matches[1];
-      string exp_user = matches[2];
-      int col = stoi(matches[3]);
+  } else if (regex_search(cmd, matches, CmdPattern)) {
+    string ingameCmd = matches[2];
 
-      if (exp_user != name) return;
-      Blackboard& bb = artist->GetBlackboard();
-      string info = "Assign user " + exp_user + " for column " + string(matches[3]);
-      cout << GetTime() << info << endl;
-      artist->SendChatMessage(info);
-      bb.Set("workCol", col);
-    }
-  } else if (cmd.find("setWorker") != string::npos) {
-    regex patternAssign("([^\\s]+) (\\d+)");
-    smatch matches;
-    if (regex_search(cmd, matches, patternAssign)) {
-      Blackboard& bb = artist->GetBlackboard();
-      int worker_num = stoi(matches[2]);
-      string info = "Setup total worker " + string(matches[2]);
-      cout << GetTime() << info << endl;
-      artist->SendChatMessage(info);
-      bb.Set("workerNum", worker_num);
-    }
-  } else if (cmd == "checkDuty") {
+    artist->SendChatCommand(ingameCmd);
+  } else if (regex_search(cmd, matches, NamePattern)) {
+    string name = artist->GetNetworkManager()->GetMyName();
+
+    artist->SendChatMessage(name);
+  } else if (regex_search(cmd, matches, SetColPattern)) {
+    string name = artist->GetNetworkManager()->GetMyName();
+    string exp_user = matches[2];
+    int col = stoi(matches[3]);
+
+    if (exp_user != name) return;
+    Blackboard& bb = artist->GetBlackboard();
+    string info = "Assign user " + exp_user + " for column " + string(matches[3]);
+
+    cout << GetTime() << info << endl;
+    artist->SendChatMessage(info);
+    bb.Set("workCol", col);
+  } else if (regex_search(cmd, matches, SetWorkerPattern)) {
+    Blackboard& bb = artist->GetBlackboard();
+    int worker_num = stoi(matches[2]);
+    string info = "Setup total worker " + string(matches[2]);
+
+    cout << GetTime() << info << endl;
+    artist->SendChatMessage(info);
+    bb.Set("workerNum", worker_num);
+  } else if (regex_search(cmd, matches, CheckDutyPattern)) {
     Blackboard& bb = artist->GetBlackboard();
     int workCol = bb.Get<int>("workCol", 0);
     int workerNum = bb.Get<int>("workerNum", 1);
     string info = "Max worker: " + to_string(workerNum) + ", work col: " + to_string(workCol);
+
     artist->SendChatMessage(info);
-  } else if (cmd == "setDefault") {
+  } else if (regex_search(cmd, matches, SetDefaultPattern)) {
     Blackboard& bb = artist->GetBlackboard();
-    string info = "Reset workCol & wirkerNum value";
+    string info = "Reset workCol & workerNum value";
+
     artist->SendChatMessage(info);
     bb.Set("workCol", 0);
     bb.Set("workerNum", 1);
-  } else if (cmd == "exchange") {
+  } else if (regex_search(cmd, matches, IngotPattern)) {
     Blackboard& bb = artist->GetBlackboard();
     string rate = bb.Get<string>("ExchangeRate", "NOT_FOUND");
     string info = "1 Villager Ingot = " + rate + " emerald.";
+
     artist->SendChatMessage(info);
-  } else if (cmd == "channel") {
+  } else if (regex_search(cmd, matches, ChannelPattern)) {
     Blackboard& bb = artist->GetBlackboard();
     string info = "Current channel: " + bb.Get<string>("ChannelNumber", "NOT_FOUND");
+
     artist->SendChatMessage(info);
+  } else if (regex_search(cmd, matches, MovePattern)) {
+    artist->GetBlackboard().Get("pathFinder", artist->finder);
+    int x = stoi(matches[2]), y = stoi(matches[3]), z = stoi(matches[4]);
+
+    FindPathAndMove(*artist, Position(x, y, z), 2, 2, 2);
   }
 }
 
---
+++ src/Regex.hpp
@@ -0,0 +1,85 @@
+#ifndef REGEX_HPP_
+#define REGEX_HPP_
+
+#include <regex>
+
+using namespace std;
+
+/*
+match 1:  < 廢土伺服器 - mcFallout.net >
+match 2:  Domain quota
+match 3:  Domain quota from online
+match 4:  Domain quota from purchase
+match 5:  Domain quota from bonus
+match 6:  Fly available time (second)
+match 7:  Fly total time (second)
+match 8:  Fly time available/total
+match 9: Infinite fly day count
+match 10: Emerald in bank
+match 11: Currency in bank
+match 12: Exchange rate
+match 13: Channel number
+match 14: In game time
+match 15: In game position
+match 16: Channel palyer count
+match 17: Online player count
+*/
+static regex FalloutTabPattern("(< 廢土伺服器 - mcFallout.net >)"
+                                "[^\\d]+(\\d+/\\d+)"
+                                "[^\\d]+(\\d+/\\d+)[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+/\\d+)[^\\d]+(\\d+)[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+)[^\\d]+(\\d+)[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+)[^\\d]+(\\d+:\\d+\\s(?:A|P)M)-[^\\d-]+(-?\\d+\\s-?\\d+\\s-?\\d+)"
+                                "[^\\d]+(\\d+)"
+                                "[^\\d]+(\\d+\\s/\\s\\d+)");
+
+static regex HungryPattern("(hungry)");
+
+static regex StartPattern("(start)");
+
+static regex StopPattern("(stop)");
+
+static regex BarPattern("(bar)");
+
+static regex CsafePattern("(csafe)");
+
+/*
+match 1: cmd
+match 2: in game command
+*/
+static regex CmdPattern("(cmd)\\s+(.+)");
+
+static regex NamePattern("(name)");
+
+/*
+match 1: setCol
+match 2: user_name
+match 3: user responsible part
+*/
+static regex SetColPattern("(setCol)\\s+(\\S+)\\s+(\\d+)");
+
+/*
+match 1: setWorker
+match 2: worker_num
+*/
+static regex SetWorkerPattern("(setWorker)\\s+(\\d+)");
+
+static regex CheckDutyPattern("(checkDuty)");
+
+static regex SetDefaultPattern("(setDefault)");
+
+static regex IngotPattern("(ingot)");
+
+static regex ChannelPattern("(channel)");
+
+/*
+match 1: move
+match 2: x position
+match 3: y position
+match 4: z position
+*/
+static regex MovePattern("(move)\\s+(-?\\d+)\\s+(-?\\d+)\\s+(-?\\d+)");
+
+#endif

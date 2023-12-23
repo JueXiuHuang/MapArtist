7107549c0243fa509cedc38d38c7711a44176785
JueXiuHuang <joshua87719@gmail.com>
2023-12-24 00:22:17 +0800

    feat: make artist code easier to read

--- src/Artist.cpp
+++ src/Artist.cpp
@@ -18,54 +18,30 @@ using namespace Botcraft;
 using namespace ProtocolCraft;
 using namespace std;
 
+void cmdHungry(Artist *artist);
+void cmdStop(smatch matches, Artist *artist);
+void cmdStart(smatch matches, Artist *artist);
+void cmdBar(Artist *artist);
+void cmdAssignment(smatch matches, Artist *artist);
+void cmdWorker(smatch matches, Artist *artist);
+void cmdDefaultSetting(Artist *artist);
+void cmdMove(smatch matches, Artist *artist);
+void cmdWaitingRoom(smatch matches, Artist *artist);
+void cmdTpSuccess(Artist *artist);
+void cmdTpHome(smatch matches, Artist *artist);
+
 void cmdHandler(string text, Artist *artist) {
   smatch matches;
   Blackboard& bb = artist->GetBlackboard();
 
   if (regex_search(text, matches, HungryPattern)) {
-    Status s = IsHungry(*artist, 15);
-
-    cout << GetTime() << "Current food: " << artist->GetEntityManager()->GetLocalPlayer()->GetFood() << endl;
-    if (s == Status::Success) {
-      artist->SendChatMessage("I'm hungry.");
-    } else {
-      artist->SendChatMessage("I'm not hungry.");
-    }
+    cmdHungry(artist);
   } else if (regex_search(text, matches, StopPattern)) {
-    string name = artist->GetNetworkManager()->GetMyName();
-    if (string(matches[2]) != "all" && string(matches[2]) != name) return;
-    
-    artist->SendChatMessage("=== BOT STOP ===");
-    // backup first, then set hasWork to false
-    artist->Backup();
-    artist->hasWork = false;
-    artist->SetBehaviourTree(nullptr);
+    cmdStop(matches, artist);
   } else if (regex_search(text, matches, StartPattern)) {
-    string name = artist->GetNetworkManager()->GetMyName();
-    if (string(matches[2]) != "all" && string(matches[2]) != name) return;
-
-    cout << GetTime() << "=== BOT START ===" << endl;
-    artist->SendChatMessage("=== BOT START ===");
-    artist->hasWork = true;
-    map<string, any> &initVal = artist->Recover();
-    artist->SetBehaviourTree(FullTree(), initVal);
+    cmdStart(matches, artist);
   } else if (regex_search(text, matches, BarPattern)) {
-    vector<bool> xCheck = artist->GetBlackboard().Get<vector<bool>>("SliceDFS.xCheck", vector(128, false));
-    int workers = artist->GetBlackboard().Get<int>("workerNum", 1);
-    int col = artist->GetBlackboard().Get<int>("workCol", 0);
-    int finish = 0;
-
-    for (int i = 0; i < xCheck.size(); i++) {
-      if (i%workers != col) continue;
-      if (xCheck[i]) finish++;
-    }
-    int ratio = finish * 20 / (xCheck.size()/workers);
-    double percent = static_cast<double>(finish) * 100 / (xCheck.size()/workers);
-
-    ostringstream bar;
-    bar << "[" << string(ratio, '#') << string(20 - ratio, '-') << "]  "
-        << fixed << setprecision(1) << percent << "%";
-    artist->SendChatMessage(bar.str());
+    cmdBar(artist);
   } else if (regex_search(text, matches, CsafePattern)) {
     artist->SendChatCommand(text);
   } else if (regex_search(text, matches, CmdPattern)) {
@@ -77,25 +53,9 @@ void cmdHandler(string text, Artist *artist) {
 
     artist->SendChatMessage(name);
   } else if (regex_search(text, matches, AssignmentPattern)) {
-    string name = artist->GetNetworkManager()->GetMyName();
-    string exp_user = matches[2];
-    int col = stoi(matches[3]);
-
-    if (exp_user != name) return;
-    string info = "Assign user " + exp_user + " for column " + string(matches[3]);
-
-    cout << GetTime() << info << endl;
-    artist->SendChatMessage(info);
-    if (artist->hasWork) bb.Set("workCol", col);
-    else artist->backup["workCol"] = col;
+    cmdAssignment(matches, artist);
   } else if (regex_search(text, matches, WorkerPattern)) {
-    int worker_num = stoi(matches[2]);
-    string info = "Setup total worker " + string(matches[2]);
-
-    cout << GetTime() << info << endl;
-    artist->SendChatMessage(info);
-    if (artist->hasWork) bb.Set("workerNum", worker_num);
-    else artist->backup["workerNum"] = worker_num;
+    cmdWorker(matches, artist);
   } else if (regex_search(text, matches, DutyPattern)) {
     int workCol = (artist->hasWork) ? bb.Get<int>("workCol", 0) : any_cast<int>(artist->backup["workCol"]);
     int workerNum = (artist->hasWork) ? bb.Get<int>("workerNum", 1) : any_cast<int>(artist->backup["workerNum"]);
@@ -103,11 +63,7 @@ void cmdHandler(string text, Artist *artist) {
 
     artist->SendChatMessage(info);
   } else if (regex_search(text, matches, DefaultSettingPattern)) {
-    string info = "Reset workCol & workerNum value";
-
-    artist->SendChatMessage(info);
-    if (artist->hasWork) bb.Set("workCol", 0);
-    else artist->backup["workCol"] = 0;
+    cmdDefaultSetting(artist);
   } else if (regex_search(text, matches, IngotPattern)) {
     string rate = bb.Get<string>("ExchangeRate", "NOT_FOUND");
     string info = "1 Villager Ingot = " + rate + " emerald.";
@@ -118,47 +74,13 @@ void cmdHandler(string text, Artist *artist) {
 
     artist->SendChatMessage(info);
   } else if (regex_search(text, matches, MovePattern)) {
-    int x = stoi(matches[2]), y = stoi(matches[3]), z = stoi(matches[4]);
-
-    if (matches[1] == "bmove") {
-      artist->SetBehaviourTree(BMoveTree(Position(x, y, z)));
-    } else {
-      map<string, any> &initVal = artist->Recover();
-      artist->SetBehaviourTree(MoveTree(Position(x, y, z)), initVal);
-    }
+    cmdMove(matches, artist);
   } else if (regex_search(text, matches, WaitingRoomPattern)) {
-    if (matches[1] == "wait") {
-      artist->Backup();
-      artist->inWaitingRoom = true;
-      artist->SetBehaviourTree(nullptr);
-      cout << GetTime() << "Transfered to waiting room, stop working..." << endl;
-      // artist->SendChatMessage("Transfered to waiting room, stop working...");
-    } else {
-      if (artist->inWaitingRoom) artist->waitTpFinish = true;
-
-      // string info = "Transfered to channel " + string(matches[1]);
-
-      // artist->SendChatMessage(info);
-    }
+    cmdWaitingRoom(matches, artist);
   } else if (regex_search(text, matches, TpSuccessPattern)) {
-    if (artist->waitTpFinish) {
-      artist->inWaitingRoom = false;
-      artist->waitTpFinish = false;
-      cout << GetTime() << "Back to work..." << endl;
-      // artist->SendChatMessage("Back to work...");
-      if (artist->hasWork) {
-        artist->hasWork = true;
-        map<string, any> &initVal = artist->Recover();
-        artist->SetBehaviourTree(FullTree(), initVal);
-      }
-    }
+    cmdTpSuccess(artist);
   } else if (regex_search(text, matches, TpHomePattern)) {
-    string homeName = bb.Get<string>("home", "mapart");
-
-    if (string(matches[1]) == homeName) {
-      bb.Set("GetHome", true);
-      cout << GetTime() << "Bot teleport to home." << endl;
-    }
+    cmdTpHome(matches, artist);
   }
 }
 
@@ -262,4 +184,142 @@ void Artist::Handle(ClientboundPlayerPositionPacket &msg) {
   }
   cout << GetTime() << "Finish notifying all listeners" << endl;
   cout << "=========================" << endl;
+}
+
+void cmdHungry(Artist *artist) {
+  Status s = IsHungry(*artist, 15);
+
+  cout << GetTime() << "Current food: " << artist->GetEntityManager()->GetLocalPlayer()->GetFood() << endl;
+  if (s == Status::Success) {
+    artist->SendChatMessage("I'm hungry.");
+  } else {
+    artist->SendChatMessage("I'm not hungry.");
+  }
+}
+
+void cmdStop(smatch matches, Artist *artist) {
+  string name = artist->GetNetworkManager()->GetMyName();
+  if (string(matches[2]) != "all" && string(matches[2]) != name) return;
+  
+  artist->SendChatMessage("=== BOT STOP ===");
+  // backup first, then set hasWork to false
+  artist->Backup();
+  artist->hasWork = false;
+  artist->SetBehaviourTree(nullptr);
+}
+
+void cmdStart(smatch matches, Artist *artist) {
+  string name = artist->GetNetworkManager()->GetMyName();
+  if (string(matches[2]) != "all" && string(matches[2]) != name) return;
+
+  cout << GetTime() << "=== BOT START ===" << endl;
+  artist->SendChatMessage("=== BOT START ===");
+  artist->hasWork = true;
+  map<string, any> &initVal = artist->Recover();
+  artist->SetBehaviourTree(FullTree(), initVal);
+}
+
+void cmdBar(Artist *artist) {
+  vector<bool> xCheck = artist->GetBlackboard().Get<vector<bool>>("SliceDFS.xCheck", vector(128, false));
+  int workers = artist->GetBlackboard().Get<int>("workerNum", 1);
+  int col = artist->GetBlackboard().Get<int>("workCol", 0);
+  int finish = 0;
+
+  for (int i = 0; i < xCheck.size(); i++) {
+    if (i%workers != col) continue;
+    if (xCheck[i]) finish++;
+  }
+  int ratio = finish * 20 / (xCheck.size()/workers);
+  double percent = static_cast<double>(finish) * 100 / (xCheck.size()/workers);
+
+  ostringstream bar;
+  bar << "[" << string(ratio, '#') << string(20 - ratio, '-') << "]  "
+      << fixed << setprecision(1) << percent << "%";
+  artist->SendChatMessage(bar.str());
+}
+
+void cmdAssignment(smatch matches, Artist *artist) {
+  Blackboard& bb = artist->GetBlackboard();
+  string name = artist->GetNetworkManager()->GetMyName();
+  string exp_user = matches[2];
+  int col = stoi(matches[3]);
+
+  if (exp_user != name) return;
+  string info = "Assign user " + exp_user + " for column " + string(matches[3]);
+
+  cout << GetTime() << info << endl;
+  artist->SendChatMessage(info);
+  if (artist->hasWork) bb.Set("workCol", col);
+  else artist->backup["workCol"] = col;
+}
+
+void cmdWorker(smatch matches, Artist *artist) {
+  Blackboard& bb = artist->GetBlackboard();
+  int worker_num = stoi(matches[2]);
+  string info = "Setup total worker " + string(matches[2]);
+
+  cout << GetTime() << info << endl;
+  artist->SendChatMessage(info);
+  if (artist->hasWork) bb.Set("workerNum", worker_num);
+  else artist->backup["workerNum"] = worker_num;
+}
+
+void cmdDefaultSetting(Artist *artist) {
+  Blackboard& bb = artist->GetBlackboard();
+  string info = "Reset workCol & workerNum value";
+
+  artist->SendChatMessage(info);
+  if (artist->hasWork) bb.Set("workCol", 0);
+  else artist->backup["workCol"] = 0;
+}
+
+void cmdMove(smatch matches, Artist *artist) {
+  int x = stoi(matches[2]), y = stoi(matches[3]), z = stoi(matches[4]);
+
+  if (matches[1] == "bmove") {
+    artist->SetBehaviourTree(BMoveTree(Position(x, y, z)));
+  } else {
+    map<string, any> &initVal = artist->Recover();
+    artist->SetBehaviourTree(MoveTree(Position(x, y, z)), initVal);
+  }
+}
+
+void cmdWaitingRoom(smatch matches, Artist *artist) {
+  if (matches[1] == "wait") {
+    artist->Backup();
+    artist->inWaitingRoom = true;
+    artist->SetBehaviourTree(nullptr);
+    cout << GetTime() << "Transfered to waiting room, stop working..." << endl;
+    // artist->SendChatMessage("Transfered to waiting room, stop working...");
+  } else {
+    if (artist->inWaitingRoom) artist->waitTpFinish = true;
+
+    // string info = "Transfered to channel " + string(matches[1]);
+
+    // artist->SendChatMessage(info);
+  }
+}
+
+void cmdTpSuccess(Artist *artist) {
+  if (artist->waitTpFinish) {
+    artist->inWaitingRoom = false;
+    artist->waitTpFinish = false;
+    cout << GetTime() << "Back to work..." << endl;
+    // artist->SendChatMessage("Back to work...");
+    if (artist->hasWork) {
+      artist->hasWork = true;
+      map<string, any> &initVal = artist->Recover();
+      artist->SetBehaviourTree(FullTree(), initVal);
+    }
+  }
+}
+
+void cmdTpHome(smatch matches, Artist *artist) {
+  Blackboard& bb = artist->GetBlackboard();
+  string homeName = bb.Get<string>("home", "mapart");
+
+  if (string(matches[1]) == homeName) {
+    bb.Set("GetHome", true);
+    cout << GetTime() << "Bot teleport to home." << endl;
+  }
 }

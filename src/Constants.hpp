71c2700d250055563d61348203c7596cee611e48
JueXiuHuang <joshua87719@gmail.com>
2024-03-01 00:45:04 +0800

    feat: add constant file

---
+++ src/Constants.hpp
@@ -0,0 +1,63 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+#include <string>
+
+// dcchannel
+const std::string KeyDcChanID = "discord_channel_id";
+// use.dpp
+const std::string KeyUseDc = "discord_available";
+// anchor
+const std::string KeyAnchor = "anchor";
+// dctoken
+const std::string KeyDcToken = "discord_token";
+// nbt
+const std::string KeyNbt = "nbt";
+// tempblock
+const std::string KeyTmpBlock = "template_block";
+// prioritize
+const std::string KeyAlgo = "prioritize_algorithm";
+// home
+const std::string KeyHomeCmd = "home_command";
+// retry
+const std::string KeyRetry = "retry_times";
+// neighbor
+const std::string KeyNeighbor = "neighbors";
+// Config.loaded
+const std::string KeyConfigured = "config_loaded";
+// ExchangeRate
+const std::string KeyIngotRate = "fallout_ingot_exchange_rate";
+// ChannelNumber
+const std::string KeyCurrChNum = "fallout_current_channel_number";
+// CurrentPos
+const std::string KeyCurrPos = "current_position";
+// Header
+const std::string KeyHeader = "fallout_header";
+// workerNum
+const std::string KeyWorkerCount = "total_worker_count";
+// workCol
+const std::string KeyWorkCol = "player_assigned_column";
+// GetHome
+const std::string KeyBotGetHome = "bot_get_home";
+// Task.prioritized
+const std::string KeyTaskQueued = "task_already_queued";
+// qTaskPosition
+const std::string KeyTaskPosQ = "task_position_queue";
+// qTaskType
+const std::string KeyTaskTypeQ = "task_type_queue";
+// qTaskName
+const std::string KeyTaskNameQ = "task_block_name_queue";
+// Structure.start
+const std::string KeyNbtStart = "nbt_structure_start_absolute_position";
+// Structure.end
+const std::string KeyNbtEnd = "nbt_structure_end_absolute_position";
+// Structure.target
+const std::string KeyNbtTarget = "nbt_relative_position_and_block_id_mapping";
+// Structure.palette
+const std::string KeyNbtPalette = "nbt_block_id_and_block_name_mapping";
+// map_memory
+const std::string KeyMapMemo = "map_memory";
+// SliceDFS.xCheck
+const std::string KeyXCheck = "x_check_array_for_slice_dfs_progress_display";
+// 
+
+#endif

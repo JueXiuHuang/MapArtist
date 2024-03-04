4b900bb6776aaf985147a8f3ffd9bcc1822ae4fa
ldslds449 <ldslds449@gmail.com>
2024-03-04 21:50:53 +0800

    🐛 Fix toml include bug

--- cmake/tomlplusplus.cmake
+++ cmake/tomlplusplus.cmake
@@ -4,7 +4,7 @@ set(TOMLPLUSPLUS_GIT_REPOSITORY "https://github.com/marzer/tomlplusplus")
 set(TOMLPLUSPLUS_GIT_BRANCH "master")
 
 set(TOMLPLUSPLUS_SRC_PATH "${CMAKE_CURRENT_SOURCE_DIR}/dependency/tomlplusplus")
-set(TOMLPLUSPLUS_HEADER_PATH "${TOMLPLUSPLUS_SRC_PATH}/toml++/include")
+set(TOMLPLUSPLUS_HEADER_PATH "${TOMLPLUSPLUS_SRC_PATH}/include")
 
 if(NOT EXISTS "${TOMLPLUSPLUS_SRC_PATH}/.git")
   message(STATUS "Update submodule")
@@ -14,8 +14,8 @@ if(NOT EXISTS "${TOMLPLUSPLUS_SRC_PATH}/.git")
 endif()
 
 file(GLOB_RECURSE TOMLPLUSPLUS_HEADER CONFIGURE_DEPENDS
-  "${TOMLPLUSPLUS_HEADER_PATH}/*.h"
-  "${TOMLPLUSPLUS_HEADER_PATH}/*.hpp"
+  "${TOMLPLUSPLUS_HEADER_PATH}/toml++/*.h"
+  "${TOMLPLUSPLUS_HEADER_PATH}/toml++/*.hpp"
 )
 
 include_directories(${TOMLPLUSPLUS_HEADER_PATH})

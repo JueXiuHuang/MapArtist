0f7e757d81ddde83d62db65a51d8673f58565f57
JueXiuHuang <joshua87719@gmail.com>
2024-03-06 01:11:21 +0800

    feat: update batch file

--- bin/_run_login.bat
+++ bin/_run_login.bat
@@ -1,3 +1,3 @@
 IF EXIST log_online.txt DEL /F log_online.txt
-mapArtist.exe -a sg.mcfallout.net -m -c config_online.txt | tee -a log_online.txt
+mapArtist.exe -a sg.mcfallout.net -m -c config_online.toml | tee -a log_online.txt
 pause
